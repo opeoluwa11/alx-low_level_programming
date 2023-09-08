c705c6bb25cb77c675b5cc7fde230b249d74d835
opeoluwa11 <adebajoopeoluwa@gmail.com>
2023-09-08 15:48:52 -0700

    100-print_comb3.c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+/**
+ * main - prints all possible combinations of single-digit numbers
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int x, y;
+
+	x = 48;
+
+	while (x <= 58)
+	{
+		y = 49;
+		while ( y <= 57;)
+		{
+			if (y > x){
+				putchar(x);
+				putchar(y);
+				if (x != 56 || y != 57)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+			y++;
+		}
+		
+		x++;
+	}
+
+	putchar('\n');
+
+	return (0);
+}
