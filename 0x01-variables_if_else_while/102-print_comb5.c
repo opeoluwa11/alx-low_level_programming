d18c25cff9d7f18714743c3718904419ed8502f9
opeoluwa11 <adebajoopeoluwa@gmail.com>
2023-09-08 16:32:25 -0700

    102-print_comb5.c

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+/**
+ * main - prints all possible combinations of single-digit numbers
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int x, y;
+
+	x = 0;
+
+	while (x < 100)
+	{
+		y = 0
+		while (y < 100)
+		{
+			if (x < y)
+			{
+				putchar((x / 10) + 48);
+				putchar((x % 10) + 48);
+				putchar(' ');
+				putchar((y / 10) + 48);
+				putchar((y % 10) + 48);
+				if (x != 98 || y != 99)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+			y++;
+		}
+		x++;
+
+	}
+
+	putchar('\n');
+
+	return (0);
+}
