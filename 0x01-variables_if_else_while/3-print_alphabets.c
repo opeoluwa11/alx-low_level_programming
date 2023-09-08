fdddec43f7d89576868db3d8a605687a7e883fea
opeoluwa11 <adebajoopeoluwa@gmail.com>
2023-09-08 13:05:44 -0700

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+* main - Program entry point
+* Program will write English alphabet
+* variable n each time it is executed
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+	char n;
+
+	n = 'a';
+	while(n <= 'z')
+	{
+		putchar(n);
+		n++;
+	}
+
+	n = 'A'
+	while(n <= 'Z')
+	{
+		putchar(n);
+		n++;
+	}
+	putchar('\n');
+	return (0);
+}
