5f11aec60e0fb6a5e295af17d10e09ded5ce4a29
opeoluwa11 <adebajoopeoluwa@gmail.com>
2023-09-08 13:16:49 -0700

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+* main - Program entry point
+* Program will write English alphabet
+* variable n each time it is executed
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+	char n;
+
+	n = 'a';
+	while(n <= 'z');
+	{
+		if (n !='e' && n !='q')
+		{
+			putchar(n);
+			n++;
+
+		}
+	}
+	putchar('\n');
+	return (0);
+}
