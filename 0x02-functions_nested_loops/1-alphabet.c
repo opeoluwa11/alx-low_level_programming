c2b15d1dd13cbde0816c1bcc9b54096cff0c43d1
opeoluwa11 <adebajoopeoluwa@gmail.com>
2023-09-12 15:46:34 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_alphabet - and all lowercase
+ *
+ * Return: Always 0
+ */
+void print_alphabet(void)
+{
+	char alphabet;
+
+	for (alphabet = 'a'; calphabet <= 'z'; alphabet++)
+	{
+		_putchar(c);
+	}
+_putchar('\n');
+
+return (0);
+}
