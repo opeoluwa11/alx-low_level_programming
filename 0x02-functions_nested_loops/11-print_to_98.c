02fcce9ff37653d049361807a99df5350962d7cf
opeoluwa11 <adebajoopeoluwa@gmail.com>
2023-09-12 16:46:18 -0700

    11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -16,16 +16,16 @@ void print_to_98(int n)
 		{
 			if (i != 98)
 				printf("%d, ", i);
-			if (i == 98)
+			else if (i == 98)
 				printf("%d\n", i);
 		}
-	} if (n >= 98)
+	} else if (n >= 98)
 	{
 		for (j = n; j >= 98; j--)
 		{
 			if (j != 98)
 				printf("%d, ", j);
-			if (j == 98)
+			else if (j == 98)
 				printf("%d\n", j);
 		}
 	}
