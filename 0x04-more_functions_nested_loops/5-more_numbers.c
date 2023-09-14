3ecc3ca0117efe1fafaf5cb608a6d7d74380e6e2
opeoluwa11 <adebajoopeoluwa@gmail.com>
2023-09-14 13:59:07 -0700

    new file

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,28 +1,24 @@
-#include "holberton.h"
-#include <stdio.h>
+#include "main.h"
 
 /**
- * more_numbers - Print numbers between 0 to 14 incl.
+ * more_numbers - function that prints 10 times the numbers, from 0 to 14
+ * followed by a new line.
  *
- * Return: Void.
  */
+
 void more_numbers(void)
 {
-	int i;
-	int x;
-
-	for (x = 0; x < 10; x++)
-	{
-		i = 0;
-		while (i <= 14)
-		{
-			if (i >= 10)
-			{
-				_putchar(i / 10 + '0');
-			}
-				_putchar(i % 10 + '0');
-			i++;
-		}
-	_putchar('\n');
-	}
+int i, j;
+for (i = 1; i <= 10; i++)
+{
+for (j = 0; j <= 14; j++)
+{
+if (j >= 10)
+{
+_putchar((j / 10) + '0');
+}
+_putchar((j % 10) + '0');
+}
+_putchar('\n');
+}
 }
